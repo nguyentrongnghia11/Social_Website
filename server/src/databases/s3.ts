fe5bd5b018890e84eab60b4b8dc245f792c01b35
nghia1501 <nghianguyen15012004@gmail.com>
2026-03-22 14:20:20 +0700

    re-deploy

--- server/src/databases/s3.ts
+++ server/src/databases/s3.ts
@@ -8,10 +8,10 @@ export const s3Client = new S3Client({
     //     accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
     //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
     // },
+
     requestChecksumCalculation: 'WHEN_REQUIRED'
 });
 
-// S3 Bucket name from environment
 export const BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME || '';
 
 export default s3Client;
