3f0c0a213500afcabbdb592ae25d69354e0e4911
nghia1501 <nghianguyen15012004@gmail.com>
2026-03-22 14:33:33 +0700

    re-deploy2

--- server/src/databases/s3.ts
+++ server/src/databases/s3.ts
@@ -8,7 +8,6 @@ export const s3Client = new S3Client({
     //     accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
     //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
     // },
-
     requestChecksumCalculation: 'WHEN_REQUIRED'
 });
 
