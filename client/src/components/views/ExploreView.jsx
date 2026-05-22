702ea44211d846d28e74abac7fcb3a0bde3d8a11
nghia1501 <nghianguyen15012004@gmail.com>
2026-05-22 22:44:12 +0700

    feat: initialize project documentation and add core UI components including GridLayout and PostCard for the explore view.

--- client/src/components/views/ExploreView.jsx
+++ client/src/components/views/ExploreView.jsx
@@ -17,22 +17,26 @@ const ExploreView = () => {
   return (
     <Box>
       <Container maxWidth="xl">
-        <Grid container spacing={2}>
-          <Grid item sx={{ display: { xs: "none", lg: "block" }, width: 250, mt: 10 }}>
+        <Grid container spacing={2} wrap="nowrap" alignItems="flex-start">
+          <Grid item sx={{ display: { xs: "none", lg: "block" }, width: 250, mt: 10, flexShrink: 0 }}>
             <BannerDisplay position="left" />
           </Grid>
 
-          <Grid item xs={12} lg sx={{ flexGrow: 1 }}>
+          <Grid item xs={12} lg sx={{ flexGrow: 1, minWidth: 0 }}>
             <Navbar />
-            <GridLayout
-              left={<PostBrowser createPost contentType="posts" />}
-              right={<Sidebar />}
-            />
+            <Grid container spacing={2} sx={{ mt: 0 }} wrap="nowrap">
+              <Grid item xs={8}>
+                <PostBrowser createPost contentType="posts" />
+              </Grid>
+              <Grid item xs={4}>
+                <Sidebar />
+              </Grid>
+            </Grid>
           </Grid>
-
-          <Grid item sx={{ display: { xs: "none", lg: "block" }, width: 250, mt: 10 }}>
+          {/* 
+          <Grid item sx={{ display: { xs: "none", lg: "block" }, width: 250, mt: 10, flexShrink: 0 }}>
             <BannerDisplay position="right" />
-          </Grid>
+          </Grid> */}
         </Grid>
       </Container>
     </Box>
